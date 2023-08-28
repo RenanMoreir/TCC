f1d114318b20578780013fa4954bef426f416145
Telini07 <brunotelirocha51@gmail.com>
2023-08-28 09:24:27 -0300

    2808

--- page/config/altera_dados.php
+++ page/config/altera_dados.php
@@ -4,33 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/config/estiloAltera.css">
+    <link rel="stylesheet" href="../../style/config/estiloAltera.css">
     <title>Alterar Dados</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="img\favicon.ico">
 </head>
-<body>
-<header>
-        <div class="top-header">
-            <nav>
-                <a class="logo" href="#">MatchPet</a>
-                <div class="mobile-menu">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-                <ul class="nav-list">
-                    <li><a href="#sobre" target="_self">Sobre</a></li>
-                    <li><a href="#contato" target="_self">Contato</a></li>
-                    <li><a href="page/login/index.html" target="_self">Entrar</a></li>
-                    <li><a href="page/register/index.html" target="_self" id="principal">Cadastre-se</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>  
+<body> 
     <section>  
     <div class="forms">
-    <p>Cadastro de Usuário</p>
+    <h1>Configuração de Usuário</h1>
     <form action="altera_usuario_exe.php" method="post">
         <input type="hidden" name="id_usuario" >
         <div>
@@ -52,7 +33,7 @@
         <div>
             <input type="file" name="foto" id="foto" accept="image/*">
         </div>
-        <input type="submit" value="Salvar">
+        <input id="enviar" type="submit" value="Salvar">
     </form>
     </div>
     </section>
