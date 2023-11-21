0524cfee48c1c1a43660b24592ca48c86386a9aa
Renan Moreira <renanrodriguesmoreira@hotmail.com>
2023-11-21 11:44:49 -0300

    adas

--- process/aparecer_animal.php
+++ process/aparecer_animal.php
@@ -1,13 +1,45 @@
 <?php
-include("connection/connect.php");
+    include("connection/connect.php");
+    require_once '../core/conexao_mysql.php';
+    require_once '../core/sql.php';
+    require_once '../core/mysql.php';
 
-$id = $_COOKIE["ID"];
+    /*
+    $id = $_COOKIE["ID"]; 
 
-$getAnimal = $con->prepare("SELECT Id_animal, Nome, Raca, Porte, Cor, Especie, Sexo, Pelagen,
-                            Idade, Descricao FROM animal WHERE FK_id_abrigo LIKE ?");
-$getAnimal->bind_param("i", $id);
-$getAnimal->execute();
-$user = $getAnimal->get_result()->fetch_assoc();
+    $getAnimal = $con->prepare("SELECT Id_animal, Nome, Raca, Porte, Cor, Especie, Sexo, Pelagen,
+                                Idade, Descricao FROM animal WHERE FK_id_abrigo LIKE ?");
+    $getAnimal->bind_param("i", $id);
+    $getAnimal->execute();
+    $user = $getAnimal->get_result()->fetch_assoc();
+    */
+
+    $id = $_COOKIE["ID"]; 
+    $criterio = [
+        ['FK_id_abrigo', '=', $id]
+    ];
+
+    $animais = buscar(
+        'animal',
+        [
+            'Id_animal',
+            'Nome',
+            'Raca',
+            'Porte',
+            'Cor',
+            'Especie',
+            'Sexo',
+            'Pelagem',
+            'Idade',
+            'Descricao'
+        ],
+        $criterio    
+    );
+
+    foreach($animais as $animal)
+    {
+        print_r($animal);
+    }
 
 
 ?>
