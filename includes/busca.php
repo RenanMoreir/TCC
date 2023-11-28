aed8867e4d8cfef8a0db06a2d44e5808d8bfa867
Telini07 <brunotelinirocha51@gmail.com>
2023-11-28 11:34:33 -0300

    votentaimplementaobusca

--- includes/busca.php
+++ includes/busca.php
@@ -1,15 +1,18 @@
 <?php
 include("../process/connection/connect.php");
-
-function buscaAnimais($conexao, $user) 
-{
+// a fazer: user a ser buscado deve ser o user logado
+// teste executado com foreach
+// em casa vou tentar implementar, mas pra isso vou fazer mudanças radicais no codigo do feed, se der certo eu aviso
+function buscaAnimais($conexao, $user) {
+    $nome = $user['nome'];
+    $raca = $user['raca'];
     $pelagem = $user['pelagem'];
     $porte = $user['porte'];
     $cor = $user['cor'];
 
-    $query = "SELECT * FROM animal WHERE pelagem = ? AND porte = ? AND cor = ?";
+    $query = "SELECT * FROM animal WHERE nome = ? AND raca = ? AND pelagem = ? AND porte = ? AND cor = ?";
     $stmt = $conexao->prepare($query);
-    $stmt->bind_param("sss", $pelagem, $porte, $cor);
+    $stmt->bind_param("sssss", $nome, $raca, $pelagem, $porte, $cor);
     $stmt->execute();
 
     $result = $stmt->get_result();
